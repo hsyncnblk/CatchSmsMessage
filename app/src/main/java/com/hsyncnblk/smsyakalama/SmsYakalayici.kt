873940b74f97d7bc0381b3f8da2a43a8197296de
huseyin cinibulak <huseyincnblk@gmail.com>
2023-12-19 23:21:50 +0300

    catch SmsMessage

--- app/src/main/java/com/hsyncnblk/smsyakalama/SmsYakalayici.kt
+++ app/src/main/java/com/hsyncnblk/smsyakalama/SmsYakalayici.kt
@@ -3,10 +3,34 @@ package com.hsyncnblk.smsyakalama
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Build
+import android.telephony.SmsMessage
 import android.widget.Toast
 
 class SmsYakalayici : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
-        Toast.makeText(context,"sms geldi",Toast.LENGTH_SHORT).show()
+
+        val b = intent?.extras
+
+        if (b !=null){
+
+            val pdusObj = b.get("pdus") as Array<Any>
+
+            for (i in pdusObj.indices){
+                val guncelMesaj :SmsMessage
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                    val format = b.getString("format")
+                    guncelMesaj = SmsMessage.createFromPdu(pdusObj[i] as ByteArray,format)
+                }else{
+                    guncelMesaj = SmsMessage.createFromPdu(pdusObj[i] as ByteArray)
+                }
+                val telNo = guncelMesaj.displayOriginatingAddress
+                val mesaj = guncelMesaj.displayMessageBody
+
+                Toast.makeText(context,"$telNo - $mesaj",Toast.LENGTH_SHORT).show()
+            }
+
+        }
     }
 }
